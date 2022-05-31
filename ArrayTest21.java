6785405adcc9fed79b3ef90d71bacd905c23328a
rohan1998 <rohansarkarmsit@gmail.com>
2022-05-31 12:40:46 +0530

    first push

---
+++ ArrayTest21.java
@@ -0,0 +1,13 @@
+
+public class ArrayTest21 {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4};
+	int sum = 0;
+        for(int i = 0;i<array.length;i++)
+        {
+         	sum =sum+array[i];  
+        }
+	System.out.print(sum);
+
+    }
+}
